1a246d3a94634c00b04a91e4a1f773348b156f0b
Agustin <agustin.ledezma@ucb.edu.bo>
2024-04-04 20:56:55 -0400

    1

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("confirmarBtn").addEventListener("click", function() {
+    document.getElementById("confirmacion").innerText = "¡Gracias por confirmar tu asistencia! Esperamos verte en el picnic.";
+});
